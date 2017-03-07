01c3cfaae6f32fcbff95e024be2782de9456d8dc
MatiasMikkola12 <matu.mikkola@gmail.com>
2017-03-07 13:31:29 -0700

    Update App.js
    add comments

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ var TodoItems = React.createClass({
   render: function() {
     var todoEntries = this.props.entries;
 
+    // add new tasks through the key, which refers to the Date of creation
     function createTasks(item) {
     return <li key={item.key}>{item.text}</li>
   }
 
     var listItems = todoEntries.map(createTasks);
 
+    // create a <ul> element and display all list items
     return (
       <ul className="theList">
         {listItems}
@@ -22,12 +24,14 @@ var TodoItems = React.createClass({
 
 var TodoList = React.createClass({
 
+  // initially there is nothing on the list => so the items array is empty and state is empty
   getInitialState: function() {
     return {
       items: []
     };
   },
-
+  
+  // if the addItem function is called, push the value and Date (key) to the array
   addItem: function(e) {
     var itemArray = this.state.items;
 
@@ -37,7 +41,8 @@ var TodoList = React.createClass({
         key: Date.now()
       }
     );
-
+    
+    // display itemArray, which now has new item for the array, switches the state to have arrayitems
     this.setState({
       items: itemArray
     });
@@ -47,7 +52,7 @@ var TodoList = React.createClass({
     e.preventDefault();
   },
 
-
+  // create a form to add listItems to be displayed. The <TodoItems entries> displays the items of the current array
   render: function() {
       return (
         <div className="todoListMain">
